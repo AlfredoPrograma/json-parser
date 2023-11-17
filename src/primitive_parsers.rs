618641b6ad5f2a3fd213aeffe86297228a2c19dc
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2023-11-17 10:33:33 -0400

    refactor(primitive parsers): improve parse integer and parse float functions
    simplify integer parsing using digit1 parser from nom
    remove completely parse float function and replace it with float parser from nom

--- src/primitive_parsers.rs
+++ src/primitive_parsers.rs
@@ -1,8 +1,8 @@
 use nom::{
-    bytes::complete::{take_until, take_while1},
-    character::complete::char,
+    bytes::complete::take_until,
+    character::complete::{char, digit1},
     combinator::opt,
-    sequence::{delimited, preceded},
+    sequence::delimited,
     IResult, Parser,
 };
 
@@ -12,34 +12,15 @@ pub fn parse_string(input: &str) -> IResult<&str, &str> {
 }
 
 pub fn parse_integer(input: &str) -> IResult<&str, i32> {
-    opt(char('-')).parse(input).and_then(|(next, sign)| {
-        take_while1(|c: char| c.is_numeric())
-            .parse(next)
-            .map(|(next, n)| match sign {
-                Some(_) => (next, n.parse::<i32>().unwrap() * (-1)),
-                None => (next, n.parse::<i32>().unwrap()),
-            })
-    })
-}
+    opt(char('-')).parse(input).and_then(|(next_input, sign)| {
+        digit1(next_input).map(|(next_input, number)| {
+            let parsed_number = number.parse::<i32>().unwrap();
 
-pub fn parse_float(input: &str) -> IResult<&str, f64> {
-    opt(char('-')).parse(input).and_then(|(next, sign)| {
-        take_while1(|c: char| c.is_numeric())
-            .parse(next)
-            .and_then(|(float, integer)| {
-                preceded(char('.'), take_while1(|c: char| c.is_numeric()))
-                    .parse(float)
-                    .map(|(next, f)| match sign {
-                        Some(_) => {
-                            let value = format!("{}.{}", integer, f);
-                            (next, value.parse::<f64>().unwrap() * (-1.0))
-                        }
-                        None => {
-                            let value = format!("{}.{}", integer, f);
-                            (next, value.parse::<f64>().unwrap())
-                        }
-                    })
-            })
+            match sign {
+                Some(_) => (next_input, parsed_number * (-1)),
+                None => (next_input, parsed_number),
+            }
+        })
     })
 }
 
@@ -47,7 +28,7 @@ pub fn parse_float(input: &str) -> IResult<&str, f64> {
 mod tests {
     use nom::error;
 
-    use super::{parse_float, parse_integer, parse_string};
+    use super::{parse_integer, parse_string};
 
     #[test]
     fn test_parse_string() {
@@ -83,40 +64,14 @@ mod tests {
             parse_integer(""),
             Err(nom::Err::Error(nom::error::Error::new(
                 "",
-                error::ErrorKind::TakeWhile1
+                error::ErrorKind::Digit
             )))
         );
         assert_eq!(
             parse_integer("not a number"),
             Err(nom::Err::Error(nom::error::Error::new(
                 "not a number",
-                error::ErrorKind::TakeWhile1
-            )))
-        )
-    }
-
-    #[test]
-    fn test_parse_float() {
-        assert_eq!(parse_float("10.25"), Ok(("", 10.25)));
-        assert_eq!(parse_float("-10.25"), Ok(("", -10.25)));
-        assert_eq!(
-            parse_float("-10.25 other text i dont use 123"),
-            Ok((" other text i dont use 123", -10.25))
-        );
-
-        assert_eq!(
-            parse_float(""),
-            Err(nom::Err::Error(nom::error::Error::new(
-                "",
-                error::ErrorKind::TakeWhile1
-            )))
-        );
-
-        assert_eq!(
-            parse_float("not a number"),
-            Err(nom::Err::Error(nom::error::Error::new(
-                "not a number",
-                error::ErrorKind::TakeWhile1
+                error::ErrorKind::Digit
             )))
         )
     }
