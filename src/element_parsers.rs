ba80b8295590b295a11550ced3be54b2e6d8b0c5
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2023-11-17 16:44:10 -0400

    feature(element parsers): update parsers to return a new parser via instead result value

--- src/element_parsers.rs
+++ src/element_parsers.rs
@@ -1,9 +1,35 @@
-pub fn parse_key() {}
+use nom::{bytes::complete::tag, sequence::separated_pair, IResult, Parser};
 
-pub fn parse_value() {
-    !unimplemented!("")
+use crate::primitive_parsers::parse_string;
+
+pub fn parse_key_value() -> impl FnMut(&str) -> IResult<&str, (&str, &str)> {
+    |input| separated_pair(parse_string(), tag(": "), parse_string()).parse(input)
 }
 
-pub fn parse_element() {
-    !unimplemented!()
+#[cfg(test)]
+mod tests {
+    use nom::Parser;
+
+    use crate::element_parsers::parse_key_value;
+
+    #[test]
+    fn test_parse_key() {
+        assert_eq!(
+            parse_key_value().parse("\"name\": \"Alfredo\""),
+            Ok(("", ("name", "Alfredo")))
+        );
+
+        assert_eq!(
+            parse_key_value().parse("\"name\": \"Alfredo\"\n ...other key value pairs"),
+            Ok(("\n ...other key value pairs", ("name", "Alfredo")))
+        );
+
+        assert_eq!(
+            parse_key_value().parse("not a key pair value"),
+            Err(nom::Err::Error(nom::error::Error::new(
+                "not a key pair value",
+                nom::error::ErrorKind::Char
+            )))
+        )
+    }
 }
